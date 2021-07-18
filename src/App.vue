0b74c3256a174c323150fa3968e40969e2238135
VictorSandbox <victor.w.cairo@gmail.com>
2021-07-18 15:09:24 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- https://dev-notes.eu/2018/05/passing-data-between-vue-components/ -->
+  <section>
+    <div class="container">
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+      <!-- Source Section-->
+
+      <h2 class="title is-4 mt-3">Word Translator</h2>
+      <div class="is-flex">
+        <form @submit.prevent="handleSubmit" class="m-3">
+          <div
+            class="is-flex is-flex-direction-row is-justify-content-flex-start mb-3"
+          >
+            <language-selector
+              @LanguageSelected="handleEvent($event)"
+              class="mr-2"
+            />
+
+            <language-selector @LanguageSelected="handleEvent($event)" />
+          </div>
+
+          <div
+            class="is-flex is-flex-direction-row is-justify-content-flex-start mb-3"
+          >
+            <div class="field mr-2">
+              <div class="control">
+                <input
+                  class="input"
+                  type="text"
+                  placeholder="Word to translate"
+                  @keyup="this.wordToTranslate = $event"
+                  v-model="wordToTranslate"
+                />
+              </div>
+            </div>
+
+            <div class="field">
+              <input
+                class="button is-primary"
+                type="submit"
+                value="Translate"
+              />
+              <!-- <button class="">Translate</button> -->
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <!-- Target Section  -->
+      <div class="mt-2" style="width: 50%">
+        <textarea
+          class="textarea is-medium"
+          name="target"
+          type="text"
+          cols="10"
+          rows="5"
+          v-model="translatedWord"
+        ></textarea>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LanguageSelector from "./components/LanguageSelector.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LanguageSelector,
+  },
+
+  data() {
+    return {
+      sourceLanguage: "",
+      wordToTranslate: "",
+      translatedWord: "",
+    };
+  },
+  methods: {
+    handleEvent(event) {
+      // console.log(event)
+      this.sourceLanguage = event;
+    },
+
+    handleSubmit() {
+      console.log(this.sourceLanguage);
+      if (this.sourceLanguage !== "" && this.wordToTranslate !== "") {
+        this.translatedWord =
+          "Placeholder for translated word...key for API expired has expired";
+      } else {
+        console.log("An error has occurred");
+      }
+
+      // console.log(this.sourceLanguage);
+      // console.log(this.wordToTranslate);
+      // fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170329T180255Z.3c18d2dc7b65d525.f23ed9a9efa992bded4ef96334e3c154f61d2dea&lang='+"es"+'&text='+"zapato")
+      // .then((response) => {
+      //   console.log(response);
+      // });
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css">
 </style>
---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="field">
+      <label class="label mr-2">From: </label>
+      <div class="control">
+        <div class="select">
+          <select
+            name="languages"
+            @change="LanguageSelected"
+            v-model="selected"
+          >
+            <option value="english">English</option>
+            <option value="spanish">Spanish</option>
+            <option value="german">German</option>
+            <option value="portuguese">Portuguese</option>
+            <option value="french">French</option>
+            <option value="italian">Italian</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+   
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  data() {
+    return {
+      selected: "",
+    };
+  },
+  methods: {
+    LanguageSelected() {
+      this.$emit("LanguageSelected", this.selected);
+    },
+  },
+};
+</script>
+
+<style lang="css">
+</style>
+
+
+
+// Pass label property - pending
